e2f445c424d8203d890baddbf874b183a4ec4c60
neerajadhav <neerajadhav@duck.com>
2023-08-02 14:13:55 +0530

    dashboard init + fa fa local

--- templates/plain/js/script.js
+++ templates/plain/js/script.js
@@ -1,9 +1,9 @@
-(new IntersectionObserver(function(e,o){
-    if (e[0].intersectionRatio > 0){
+(new IntersectionObserver(function (e, o) {
+    if (e[0].intersectionRatio > 0) {
         document.documentElement.removeAttribute('class');
         document.querySelector('.brandingLogo').style.maxWidth = "190px";
     } else {
-        document.documentElement.setAttribute('class','stuck');
+        document.documentElement.setAttribute('class', 'stuck');
         document.querySelector('.brandingLogo').style.maxWidth = "150px";
     };
 })).observe(document.querySelector('.trigger'));
