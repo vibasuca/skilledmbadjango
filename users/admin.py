f8efbdb2c98fcd7516469db004b0531d1a03cb23
neerajadhav <neerajadhav@duck.com>
2023-08-05 14:18:31 +0530

    Integrate dashboard frontend

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,7 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets
     fieldsets[0][1]["fields"] += ("is_student", "is_instructor")
-    fieldsets[1][1]["fields"] += ("profile_pic", "cover_pic", "bio", "phone")
+    fieldsets[1][1]["fields"] += ("profile_pic", "cover_pic", "job_title", "bio", "phone")
 
 
 admin.site.register(User, CustomUserAdmin)
