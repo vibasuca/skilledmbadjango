8ff3f6621f78c0f10e109ecf4bb829d38c2859f2
neerajadhav <neerajadhav@duck.com>
2023-08-13 12:00:28 +0530

    moving to hero

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,12 @@
 (new IntersectionObserver(function (e, o) {
+    var navbar = document.getElementById('myNavbar');
     if (e[0].intersectionRatio > 0) {
         document.documentElement.removeAttribute('class');
         document.querySelector('.brandingLogo').style.maxWidth = "190px";
+        navbar.classList.remove("sticky");
     } else {
         document.documentElement.setAttribute('class', 'stuck');
         document.querySelector('.brandingLogo').style.maxWidth = "150px";
+        navbar.classList.add("sticky");
     };
 })).observe(document.querySelector('.trigger'));
