dca591bf9bd9397a79ab7331a0040bd80d8a1e47
Om Dahale <om.g.dahale@gmail.com>
2023-08-19 14:15:58 +0530

    Add quiz attempt details page

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -6,4 +6,8 @@ app_name = "quizzes"
 urlpatterns = [
     path("mark-answer/<int:attempt_pk>/", views.mark_answer, name="mark_answer"),
     path("submit-answer/<int:attempt_pk>/", views.submit_answer, name="submit_answer"),
+    path("quiz-attempts/", views.quiz_attempts, name="quiz_attempts"),
+    path(
+        "quiz-attempts/<int:pk>/", views.quiz_attempt_detail, name="quiz_attempt_detail"
+    ),
 ]
