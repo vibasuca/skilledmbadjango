46552e91f01cb76724fa04f17dbd6e7ee72f62f7
Om Dahale <om.g.dahale@gmail.com>
2023-09-14 15:54:07 +0530

    Fix media thumbnails

--- media_library/models.py
+++ media_library/models.py
@@ -1,9 +1,14 @@
+from io import BytesIO
 import os
 import uuid
+import cv2
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.core.files.base import ContentFile
 from django.core.validators import FileExtensionValidator
 from django.db import models
+from moviepy.editor import VideoFileClip
+from PIL import Image
 
 User = get_user_model()
 
@@ -64,6 +69,11 @@ class Media(models.Model):
     file = models.FileField(
         upload_to=user_directory_path, validators=[validate_file_extension]
     )
+    thumbnail = models.ImageField(
+        upload_to="media-thumbnails/",
+        default="media_library/default_thumbnail.png",
+        blank=True,
+    )
     title = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
     alt_text = models.CharField(max_length=255, blank=True)
@@ -85,6 +95,40 @@ class Media(models.Model):
             value = value / 1073741824.0
             ext = "gb"
         return "%s %s" % (str(round(value, 2)), ext)
+
+    def save(self, *args, **kwargs):
+        is_new_instance = not self.pk
+        super().save(*args, **kwargs)
+
+        base_filename, ext = os.path.splitext(self.file.name)
+
+        if is_new_instance:
+            self.title = self.file.name.split("/")[-1]
+            if ext in [".mp4", ".avi", ".mov", ".mkv", ".wmv"]:
+                video_path = self.file.path
+                thumbnail_bytes = self.generate_thumbnail(video_path)
+                thumbnail_file = ContentFile(thumbnail_bytes, name="thumbnail.jpg")
+                self.thumbnail.save("thumbnail.jpg", thumbnail_file, save=False)
+            elif ext in [".jpg", ".jpeg", ".png", ".gif", ".bmp"]:
+                thumbnail = self.file
+                self.thumbnail.save("thumbnail.jpeg", thumbnail, save=False)
+            super().save(*args, **kwargs)
+
+    def generate_thumbnail(self, video_path):
+        clip = VideoFileClip(video_path)
+        frame = clip.get_frame(0)  # Get the first frame as a thumbnail
+
+        # Convert the frame to RGB color space
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Manually invert the colors
+        inverted_frame_rgb = 255 - frame_rgb
+
+        # Create the thumbnail from the inverted frame
+        thumbnail = Image.fromarray(inverted_frame_rgb.astype("uint8"))
+        thumbnail_io = BytesIO()
+        thumbnail.save(thumbnail_io, format="JPEG")
+        return thumbnail_io.getvalue()
 
     def __str__(self):
         return f"{self.user.username}'s media: {self.file.name}"
